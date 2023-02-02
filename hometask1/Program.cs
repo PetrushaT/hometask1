9b16fe88015f874214c4de6862821dba627daa46
PetrushaT <petrusha_ak15@nuwm.edu.ua>
2023-02-02 12:23:09 +0200

    Add project files.

---
+++ hometask1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using hometask1.CalcPropFigures;
+using hometask1.TimeDate;
+
+Figures FiguresWork1 = new Figures();
+Clock ClockWork1 = new Clock();
+/*
+ * Перетворювач дати з dd.MM.YYYY в dd/MM/YYYY
+ * ClockWork1.DateFunctionNow();
+*/
+/*
+ * Обрахування площі та перемитру прямокутника
+ * FiguresWork1.Square();
+*/
+
+/*
+ * Обрахування площі кола
+ * FiguresWork1.Circle();
+*/
+
+/* Секунди в години-хвилини-секунди
+ * ClockWork1.SecondsToTime();
+*/
+
+/* Скільки днів в році
+ * ClockWork1.PrintYearDays();
+*/
+ClockWork1.PrintYearDays();
